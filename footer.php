b45351e7009c4fad45886130c802348eb0d192ce
hamzahfauzy <hamzahfauzy97@gmail.com>
2023-11-12 01:46:11 +0700

    support tinymce

--- footer.php
+++ footer.php
@@ -10,6 +10,14 @@
 <script src="<?=asset('theme/assets/js/datatables/datatables.min.js')?>"></script>
 <script src="<?=asset('theme/assets/js/datatables/datatables.bootstrap5.min.js')?>"></script>
 <script src="<?=asset('theme/assets/js/datatables-pagingtype/full_numbers_no_ellipses.js')?>"></script>
+<script src="https://cdn.ckeditor.com/ckeditor5/40.0.0/classic/ckeditor.js"></script>
+<script>
+ClassicEditor
+        .create( document.querySelector( '.ckeditor' ) )
+        .catch( error => {
+            console.error( error );
+        } );
+</script>
 <?php foot_script() ?>
 </body>
 </html>
